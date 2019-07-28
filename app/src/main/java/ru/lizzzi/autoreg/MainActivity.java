80d26ba203012defdb16394596babbfdfba086ba
Aleksandr Shupilo <bason.89@mail.ru>
2019-07-28 15:59:13 +0300

    Немного причесал код.

--- app/src/main/java/ru/lizzzi/autoreg/MainActivity.java
+++ app/src/main/java/ru/lizzzi/autoreg/MainActivity.java
@@ -1,130 +1,110 @@
 package ru.lizzzi.autoreg;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
+public class MainActivity extends AppCompatActivity {
 
-import ru.lizzzi.autoreg.data.RegDbHelper;
+    private EditText editText;
+    private TextView textThis;
+    private TextView textRegion;
+    private TextView textOtherCode;
+    private TextView textOtherCode2;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private RegDbHelper mDbHelper = new RegDbHelper(this);
-    private EditText mEditText;
-    private TextView mTextView;
-    private TextView mTextView2;
-    private TextView mTextView3;
-    private TextView mTextView4;
+    private ViewModelMain viewModel;
+    private InputMethodManager inputMethodManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mEditText = findViewById(R.id.editText);
-        mTextView = findViewById(R.id.tv_this);
-        mTextView2 = findViewById(R.id.tv_region);
-        mTextView3 = findViewById(R.id.tv_othercod);
-        mTextView4 = findViewById(R.id.tv_text_othercod);
-
-        mTextView.setVisibility(View.INVISIBLE);
-        mTextView2.setVisibility(View.INVISIBLE);
-        mTextView3.setVisibility(View.INVISIBLE);
-        mTextView4.setVisibility(View.INVISIBLE);
-
-        mDbHelper = new RegDbHelper(this);
-
-        try {
-            mDbHelper.createDataBase();
-        } catch (IOException ioe) {
-            throw new Error("Unable to create database");
-        }
-
-        mDbHelper.openDataBase();
-    }
-
-    @Override
-    public void onClick(View view) {
-
-        String wer = mEditText.getText().toString();
-
-        if (mEditText.getText().length() == 0) {
-            ShowToast(getResources().getString(R.string.toast_enterMessage));
-
-        } else {
-            try {
-                int num = Integer.parseInt(wer);
-                if (num > 0){
-
-                    String stRegion = mDbHelper.getRegion(wer);
-                    if (stRegion != null){
-                        mTextView2.setText(stRegion);
-                        mTextView.setVisibility(View.VISIBLE);
-                        mTextView2.setVisibility(View.VISIBLE);
-                        String stCod = mDbHelper.getCod(stRegion);
-
-                        if (stCod.length() > 1){
-                            mTextView3.setText(stCod);
-                            mTextView3.setVisibility(View.VISIBLE);
-                            mTextView4.setVisibility(View.VISIBLE);
+        editText = findViewById(R.id.editText);
+        textThis = findViewById(R.id.textThis);
+        textRegion = findViewById(R.id.textRegion);
+        textOtherCode = findViewById(R.id.textOtherCode);
+        textOtherCode2 = findViewById(R.id.textOtherCode2);
+        Button buttonShowRegion = findViewById(R.id.buttonShowRegion);
+        buttonShowRegion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String codeEntered = editText.getText().toString();
+                if (codeEntered.length() == 0) {
+                    showToast(getResources().getString(R.string.toast_enterMessage));
+                    return;
+                }
+                if (Integer.parseInt(codeEntered) > 0) {
+                    String region = viewModel.getRegion(codeEntered);
+                    if (region.length() > 0) {
+                        textRegion.setText(region);
+                        textThis.setVisibility(View.VISIBLE);
+                        textRegion.setVisibility(View.VISIBLE);
+                        String codeOfRegion = viewModel.getCodeOfRegion(region);
+                        if (codeOfRegion.length() > 1) {
+                            textOtherCode.setText(codeOfRegion);
+                            textOtherCode.setVisibility(View.VISIBLE);
+                            textOtherCode2.setVisibility(View.VISIBLE);
                         }
 
                         //убираем клавиатуру после нажатия на кнопку
-                        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-                        if (imm != null) {
-                            imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+                        if (inputMethodManager != null) {
+                            inputMethodManager.hideSoftInputFromWindow(
+                                    editText.getWindowToken(),
+                                    0);
                         }
-                    }else {
-                        ShowToast(getResources().getString(R.string.toast_noRegion));
-
+                    } else {
+                        showToast(getResources().getString(R.string.toast_noRegion));
                     }
                 }else {
-                    ShowToast(getResources().getString(R.string.toast_errorSimbol));
+                    showToast(getResources().getString(R.string.toast_errorSimbol));
                 }
-
-            } catch (NumberFormatException e) {
-                ShowToast(getResources().getString(R.string.toast_errorSimbol));
             }
-        }
+        });
+
+        textThis.setVisibility(View.INVISIBLE);
+        textRegion.setVisibility(View.INVISIBLE);
+        textOtherCode.setVisibility(View.INVISIBLE);
+        textOtherCode2.setVisibility(View.INVISIBLE);
+        viewModel = ViewModelProviders.of(this).get(ViewModelMain.class);
+        viewModel.checkStorage();
+        inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
     }
 
     @Override
     public void onStart() {
-
         super.onStart();
-        if (mTextView2.length() > 1){
-            mTextView.setVisibility(View.VISIBLE);
-            mTextView2.setVisibility(View.VISIBLE);
-            if (mTextView3.length() > 1){
-                mTextView3.setVisibility(View.VISIBLE);
-                mTextView4.setVisibility(View.VISIBLE);
+        if (viewModel.getDefaultCodeOfRegion().length() > 0) {
+            textThis.setVisibility(View.VISIBLE);
+            textRegion.setVisibility(View.VISIBLE);
+            if (textOtherCode.length() > 1) {
+                textOtherCode.setVisibility(View.VISIBLE);
+                textOtherCode2.setVisibility(View.VISIBLE);
             }
-        }else {
-            mEditText.requestFocus();
-            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-            if (imm != null) {
-                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+        } else {
+            editText.requestFocus();
+            if (inputMethodManager != null) {
+                inputMethodManager.toggleSoftInput(
+                        InputMethodManager.SHOW_FORCED,
+                        InputMethodManager.HIDE_IMPLICIT_ONLY);
             }
         }
     }
 
-
-    private void ShowToast (String Massage){
-
-        mTextView2.setText("");
-        mTextView3.setText("");
-
-        mTextView.setVisibility(View.INVISIBLE);
-        mTextView2.setVisibility(View.INVISIBLE);
-        mTextView3.setVisibility(View.INVISIBLE);
-        mTextView4.setVisibility(View.INVISIBLE);
-
+    private void showToast(String Massage) {
+        textRegion.setText("");
+        textOtherCode.setText("");
+        textThis.setVisibility(View.INVISIBLE);
+        textRegion.setVisibility(View.INVISIBLE);
+        textOtherCode.setVisibility(View.INVISIBLE);
+        textOtherCode2.setVisibility(View.INVISIBLE);
         Toast toast = Toast.makeText(this, Massage,
                 Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER, 0, 0);
