2c36ee10c9e5d296d24dbe5ef526fce6355fb0cc
Aleksandr Shupilo <aleksandr.shupilo@simbirsoft.com>
2022-01-10 17:13:20 +0300

    Перевел главный экран с xml на compose

--- app/src/main/java/ru/lizzzi/autoreg/presentation/ui/MainActivity.kt
+++ app/src/main/java/ru/lizzzi/autoreg/presentation/ui/MainActivity.kt
@@ -1,13 +1,9 @@
 package ru.lizzzi.autoreg.presentation.ui
 
-import android.widget.EditText
-import android.widget.TextView
 import android.os.Bundle
 import android.widget.Toast
 import android.view.Gravity
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Button
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.lizzzi.autoreg.R
@@ -16,76 +12,33 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel: ViewModelMain by viewModels()
 
-    private var editText: EditText? = null
-    private var textRegion: TextView? = null
-    private var textOtherCodeForRegion: TextView? = null
-    private var textCode: TextView? = null
+    private val screenCallbacks = object : MainScreenCallbacks {
+        override fun getNewRegion(newRegionCode: String) {
+            viewModel.getRegion(newRegionCode)
+        }
 
-    private val inputMethodManager by lazy {
-        getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        override fun showMessage(toastTextResource: Int) {
+            showToast(toastTextResource)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MainScreen(
+                viewModelMain =  viewModel,
+                screenCallbacks = screenCallbacks
+            )
+        }
 
-        editText = findViewById(R.id.editText)
-
-        initButtonShowRegion()
-
-        textRegion = findViewById(R.id.textRegion)
-        textOtherCodeForRegion = findViewById(R.id.textOtherCodeForRegion)
-        textCode = findViewById(R.id.textCode)
-
-        setObserveForRegion()
-    }
-
-    private fun initButtonShowRegion() {
-        val buttonShowRegion = findViewById<Button>(R.id.buttonShowRegion)
-        buttonShowRegion.setOnClickListener(View.OnClickListener {
-            editText?.let {
-                val enteredCode = it.text.toString()
-                if (enteredCode.isEmpty()) {
-                    hideRegionInfo()
-                    showToast(R.string.toastEnterCodeOfRegion)
-                    return@OnClickListener
-                }
-                if (enteredCode.toInt() > 0) {
-                    getRegion(enteredCode)
-                } else {
-                    hideRegionInfo()
-                    showToast(R.string.toastErrorCode)
-                }
-            }
-        })
-    }
-
-    private fun setObserveForRegion() {
-        viewModel.liveData.observe(this) { region ->
-            if (region.regionName.isNotEmpty()) {
-                inputMethodManager.hideSoftInputFromWindow(editText?.windowToken, 0)
-                textRegion?.visibility = View.VISIBLE
-                textRegion?.text = region.regionName
-                if (region.allCodeRegion.isNotEmpty()) {
-                    textCode?.visibility = View.VISIBLE
-                    textOtherCodeForRegion?.visibility = View.VISIBLE
-                    textOtherCodeForRegion?.text = region.allCodeRegion
-                } else {
-                    hideOtherCodeInfo()
-                }
-            } else {
-                hideRegionInfo()
+        viewModel.viewState.observe(this) {
+            if (it.regionName.isEmpty()) {
                 showToast(R.string.toastNoSuchRegion)
             }
         }
     }
 
-    private fun getRegion(codeEntered: String) {
-        viewModel.getRegion(codeEntered)
-    }
-
     private fun showToast(toastTextResource: Int) {
-
         val toast = Toast.makeText(this, resources.getString(toastTextResource), Toast.LENGTH_LONG)
         with(toast) {
             this.setGravity(Gravity.CENTER, 0, 0)
@@ -93,21 +46,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun hideRegionInfo() {
-        textRegion?.let {
-            it.text = ""
-            it.visibility = View.GONE
-        }
-
-        hideOtherCodeInfo()
-    }
-
-    private fun hideOtherCodeInfo() {
-        textOtherCodeForRegion?.let{
-            it.text = ""
-            it.visibility = View.GONE
-        }
-
-        textCode?.visibility = View.GONE
-    }
 }
--- app/src/main/java/ru/lizzzi/autoreg/presentation/ui/ViewModelMain.kt
+++ app/src/main/java/ru/lizzzi/autoreg/presentation/ui/ViewModelMain.kt
@@ -5,14 +5,19 @@ import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
-import ru.lizzzi.autoreg.domain.entity.Region
 import ru.lizzzi.autoreg.domain.interactor.RegionInteractor
 import ru.lizzzi.autoreg.domain.interactor.RegionInteractorImpl
 
 class ViewModelMain : ViewModel() {
 
-    private val _liveData: MutableLiveData<Region> = MutableLiveData()
-    val liveData = _liveData
+    data class ViewState(
+        val codeRegion: String = "",
+        val regionName: String = "",
+        val allCodeRegion: String = ""
+    )
+
+    private val _viewState: MutableLiveData<ViewState> = MutableLiveData()
+    val viewState = _viewState
 
     private val regionInteractor: RegionInteractor by lazy {
         RegionInteractorImpl()
@@ -22,11 +27,23 @@ class ViewModelMain : ViewModel() {
         CompositeDisposable()
     }
 
+    init {
+        _viewState.value = ViewState()
+    }
+
     fun getRegion(regionCode: String) {
         regionInteractor.getRegion(regionCode)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { region -> liveData.postValue(region)}
+            .subscribe { region ->
+                _viewState.value?.let {
+                    _viewState.value = it.copy(
+                        codeRegion = region.codeRegion,
+                        regionName = region.regionName,
+                        allCodeRegion = region.allCodeRegion
+                    )
+                }
+            }
             .apply { compositeDisposable.add(this) }
     }
 
